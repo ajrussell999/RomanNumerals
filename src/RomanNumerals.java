c4c5e3b78dd5b16811e27d2edc676e4001790b43
arussell <ajrussell999@gmail.com>
2017-10-12 15:03:29 +0300

    Exercise 5, Roman numerals.

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,29 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
-		
+	
+	public int convert(String s)
+	{
+		int num = 0;
+		switch(s)
+		{
+		case "I": num = 1;
+		break;
+		case "V": num = 5;
+		break;
+		case "X": num = 10;
+		break;
+		case "L": num = 50;
+		break;
+		case "C": num = 100;
+		break;
+		case "D": num = 500;
+		break;
+		case "M": num = 1000;
+		break;
+		default:
+			break;
+		}
+		return num;
 	}
+	
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -6,7 +6,79 @@ public class TestRomanNumerals {
 
 	@Test
 	public void test() {
+		
+		
 		fail("Not yet implemented");
 	}
+	
+	@Test
+	public void basic_RomantoArabicTest()
+	{
+		//Arrange
+		RomanNumerals r = new RomanNumerals();
+				
+				
+		//Assert
+		assertEquals("wrong conversion",1 ,r.convert("I"));	
+	}
+	
+	{
+		//Arrange
+		RomanNumerals r = new RomanNumerals();
+				
+				
+		//Assert
+		assertEquals("wrong conversion",5 ,r.convert("V"));			
+		
+	}
+	
+	{
+		//Arrange
+		RomanNumerals r = new RomanNumerals();
+		
+		
+		//Assert
+		assertEquals("wrong conversion",10 ,r.convert("X"));
+	
+	}
+	
+	{
+		//Arrange
+		RomanNumerals r = new RomanNumerals();
+				
+				
+		//Assert
+		assertEquals("wrong conversion",50 ,r.convert("L"));
+	}
+	
+	{
+		//Arrange
+		RomanNumerals r = new RomanNumerals();
+				
+				
+		//Assert
+		assertEquals("wrong conversion",100 ,r.convert("C"));
+	}
+	
+	@Test 
+	public void test_C_is_not_50()
+	{
+		//Arrange
+				RomanNumerals r = new RomanNumerals();
+				
+				
+				//Assert
+				assertEquals("wrong conversion",50 ,r.convert("C"));
+	}
 
+	@Test
+	public void test_less_than_ten()
+	{
+		//Arrange
+				RomanNumerals r = new RomanNumerals();
+				
+				
+				//Assert
+				assertEquals("wrong conversion",8 ,r.convert("VIII"));
+	}
 }
